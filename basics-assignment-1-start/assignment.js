9bf8ef4ce3500ed2faf92216890d4ea242088b61
Aditya Shelar <I523349@WDFN34719926A>
2021-01-14 21:28:11 +0100

    first commit

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,15 @@
+let fictUserInput = 10;
+
+let result;
+
+result = 18 + fictUserInput;
+
+result = result - 5;
+
+result = result * 5;
+
+result = result / 4;
+
+alert(result);
+
+alert(fictUserInput);
