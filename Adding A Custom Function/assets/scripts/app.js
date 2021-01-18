82011ad9c99ed20be833cbab43148c1db939c2b9
Aditya Shelar <I523349@WDFN34719926A>
2021-01-18 21:22:18 +0100

    Event Listener

--- Adding A Custom Function/assets/scripts/app.js
+++ Adding A Custom Function/assets/scripts/app.js
@@ -1,16 +1,10 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2){
-    const result = num1 + num2;
-    return result;
+function add(){
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-add(1,2);
-add(5,2);
+addBtn.addEventListener('click', add);
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
