dc770e55b89436ae17b2b15db4daf9162aefb3a9
Aditya Shelar <adityashelar710@gmail.com>
2021-01-20 20:53:42 +0100

    Added comments

--- Adding A Custom Function/assets/scripts/app.js
+++ Adding A Custom Function/assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+//Gets input from the input field
 function getUserNumberInput(){
     return parseInt(userInput.value);
 }
 
+//Generated and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); // from vendor file
 }
 
 function add(){
