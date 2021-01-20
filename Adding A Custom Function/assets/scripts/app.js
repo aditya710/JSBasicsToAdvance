c034cd53392f3255fecd9d06e95e8c046f5b3cfe
Aditya Shelar <adityashelar710@gmail.com>
2021-01-20 20:49:51 +0100

    Adding Multiply and Divide function calls

--- Adding A Custom Function/assets/scripts/app.js
+++ Adding A Custom Function/assets/scripts/app.js
@@ -41,4 +41,6 @@ function divide(){
 
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
