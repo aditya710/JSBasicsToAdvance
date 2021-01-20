e7b020c8f317bde2c16f290b4c00460ac48e5d8a
Aditya Shelar <adityashelar710@gmail.com>
2021-01-20 20:47:09 +0100

    Add and Subtract

--- Adding A Custom Function/assets/scripts/app.js
+++ Adding A Custom Function/assets/scripts/app.js
@@ -41,6 +41,4 @@ function divide(){
 
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
 
