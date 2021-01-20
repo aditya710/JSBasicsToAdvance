f774ec7c1c8f71c8a162b988cd503cf1b7d459d8
Aditya Shelar <adityashelar710@gmail.com>
2021-01-20 21:02:22 +0100

    Added Re-assing variable like +=, -=, *=, /=

--- Adding A Custom Function/assets/scripts/app.js
+++ Adding A Custom Function/assets/scripts/app.js
@@ -15,7 +15,7 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
 function add(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
+    currentResult += enteredNumber;
     createAndWriteOutput('+', initialResult, enteredNumber);
     
 }
@@ -23,21 +23,21 @@ function add(){
 function subtract(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
+    currentResult -= enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
 function multiply(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
+    currentResult *= enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
 function divide(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
+    currentResult /= enteredNumber;
     createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
